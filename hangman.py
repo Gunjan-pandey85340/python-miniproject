c81d8ac9ef30eaba056579cb891f255de9141f21
Gunjan Pandey <122886879+Gunjan-pandey85340@users.noreply.github.com>
2023-05-01 18:20:17 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,53 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:/Users/gunja/AppData/Local/Programs/Python/Python311/hungman.py ==
+		 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+		 XXX    THE HUNGMAN GAME    XXX
+		 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+	Press ENTER to begin : 
+
+Loading.....					Chances remaining  :  6
+		_ O _ 
+
+Enter your guess :67
+
+ + - - - +
+ O       |
+         |
+         |
+        ===
+					Chances remaining  :  5
+		_ O _ 
+
+Enter your guess :37
+
+ + - - - +
+ O       |
+/        |
+         |
+        ===
+					Chances remaining  :  4
+		_ O _ 
+
+Enter your guess :267
+
+ + - - - +
+ O       |
+/|       |
+         |
+        ===
+					Chances remaining  :  3
+		_ O _ 
+
+Enter your guess :22
+
+ + - - - +
+ O       |
+/|\      |
+         |
+        ===
+					Chances remaining  :  2
+		_ O _ 
+
